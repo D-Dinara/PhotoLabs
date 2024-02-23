1d6805666f2ae1cf49848437e7a60c34b56b9da7
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-02-23 15:04:14 -0500

    Apply styles to PhotoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,11 +6,16 @@ import "../styles/PhotoListItem.scss";
 const PhotoListItem = (props) => {
   const { username, imageSource, id, location, profile } = props.photo;
   return (
-    <div key={id}>
-      <img src={imageSource} alt="Photo" />
-      <img src={profile} alt="Profile" />
-      <p>{username}</p>
-      <p>{location.city}, {location.country}</p>
+    <div key={id} className="photo-list__item">
+      <img src={imageSource} alt="Photo" className="photo-list__image" />
+      
+      <div className="photo-list__user-details">
+        <img src={profile} alt="Profile" className="photo-list__user-profile"/>
+        <div className="photo-list__user-info">
+          <p>{username}</p>
+          <p className="photo-list__user-location ">{location.city}, {location.country}</p>
+        </div>
+      </div>
     </div>
   )
 };
