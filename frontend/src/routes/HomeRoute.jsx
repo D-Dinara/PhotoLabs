f1541c40ce71f4b1fda0a707c2d17bcf828cf82a
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-04 12:27:37 -0400

    Implement useContext for PhotoList

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/HomeRoute.scss';
 import TopNavigation from 'components/TopNavigationBar';
 import PhotoList from 'components/PhotoList';
+import { contextProvider } from 'App';
 
-const HomeRoute = ({ photos, updateFavPhotos, toggleModal, favPhotos}) => {
+const HomeRoute = () => {
+  const { photoData } = useContext(contextProvider);
 
   return (
     <div className="home-route">
       <TopNavigation />
-      <PhotoList 
-        photos={photos} 
-        favPhotos={favPhotos} 
-        updateFavPhotos={updateFavPhotos}
-        toggleModal={toggleModal} 
-      />
+      <PhotoList photos={photoData} />
     </div>
   );
 };
