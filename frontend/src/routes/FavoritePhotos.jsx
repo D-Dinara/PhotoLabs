8afbd4c731794a5660dbe3acd6e32012c3837419
Dinara Dvortsova <dinara.dvortsova@gmail.com>
2024-06-04 12:30:23 -0400

    Implement useContext for FavoritePhotos route

--- frontend/src/routes/FavoritePhotos.jsx
+++ frontend/src/routes/FavoritePhotos.jsx
@@ -1,32 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import '../styles/FavoritePhotos.scss';
 import PhotoList from 'components/PhotoList';
 import TopNavigation from 'components/TopNavigationBar';
+import { contextProvider } from 'App';
 
-
-const FavoritePhotos = ({ topics, activeTopic, favPhotos, updateFavPhotos, toggleModal, getPhotosByTopic, showFavPhotos }) => {
+const FavoritePhotos = () => {
+  const { favPhotos } = useContext(contextProvider);
 
   return (
     <div className="favorite-photos">
-      <TopNavigation 
-        topics={topics} 
-        activeTopic={activeTopic}
-        favPhotos={favPhotos} 
-        getPhotosByTopic={getPhotosByTopic}
-        showFavPhotos={showFavPhotos}
-      />
+      <TopNavigation />
       <div className='favorite-photos__heading'>
         <h2>My favorite photos</h2>
         {favPhotos.length === 0 && <h3>No liked photos yet</h3>}
       </div>
       
-      
-      <PhotoList 
-        photos={favPhotos} 
-        favPhotos={favPhotos} 
-        updateFavPhotos={updateFavPhotos} 
-        toggleModal={toggleModal} 
-      />
+      <PhotoList photos={favPhotos} />
     </div>
   )
 };
